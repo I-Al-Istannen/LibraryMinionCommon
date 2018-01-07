6047fb926cc6d939b94203144d8a100df43d0242
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-01-07 14:20:09 +0100

    Added price, author parsing, and cover type parsing

--- src/main/kotlin/me/ialistannen/libraryminioncommon/fetching/amazon/DetailPageExtractor.kt
+++ src/main/kotlin/me/ialistannen/libraryminioncommon/fetching/amazon/DetailPageExtractor.kt
@@ -1,10 +1,14 @@
 package me.ialistannen.libraryminioncommon.fetching.amazon
 
 import me.ialistannen.libraryminioncommon.book.BookInformation
+import me.ialistannen.libraryminioncommon.book.CurrencyValue
 import me.ialistannen.libraryminioncommon.isbn.Isbn
 import me.ialistannen.libraryminioncommon.isbn.IsbnParser
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.text.NumberFormat
+import java.text.ParseException
+import java.util.*
 
 /**
  * Extracts [BookInformation] from an amazon detail page.
@@ -15,6 +19,8 @@ object DetailPageExtractor {
     private val RATING_EXTRACTION_REGEX = Regex("(\\d\\.\\d)\\s*von.+Stern.+")
     private val URL_EXTRACTION_REGEX = Regex("\"(https://.+?)\"")
     private val PAGE_EXTRACTION_REGEX = Regex("(\\d+).+Seiten")
+    private val CONTRIBUTION_CLEANUP_REGEX = Regex("([()])|(?<=\\)),")
+    private val PRICE_EXTRACTION_REGEX = Regex("(.+?)\\s*([\\d,.]+)")
 
     fun extractFromDetailPage(detailPage: Document): BookInformation? {
         return try {
@@ -28,7 +34,8 @@ object DetailPageExtractor {
                     publisher = extractPublisher(detailPage),
                     coverType = extractCoverType(detailPage),
                     coverImageUrl = extractCoverImageUrl(detailPage),
-                    author = extractAuthors(detailPage)
+                    author = extractAuthors(detailPage),
+                    price = extractPrice(detailPage)
             )
         } catch (e: Exception) {
             e.printStackTrace()
@@ -37,7 +44,32 @@ object DetailPageExtractor {
     }
 
     private fun extractAuthors(detailPage: Document): List<Pair<String, String>> {
-        return emptyList()
+        return detailPage.getElementsByClass("author")
+                .mapNotNull {
+                    val authorName = (
+                            getAuthorName(it)
+                                    ?.text()
+                                    ?: return@mapNotNull null
+                            )
+                    val contribution = (
+                            it.getElementsByClass("contribution")
+                                    .firstOrNull()
+                                    ?.text()
+                                    ?.replace(CONTRIBUTION_CLEANUP_REGEX, "")
+                                    ?: return@mapNotNull null
+                            )
+
+                    authorName to contribution
+                }
+    }
+
+    private fun getAuthorName(element: Element): Element? {
+        val nameID = element.getElementsByClass("contributorNameID")
+        if (nameID.isNotEmpty()) {
+            return nameID.firstOrNull()
+        }
+
+        return element.getElementsByTag("a").firstOrNull()
     }
 
     private fun extractCoverImageUrl(detailPage: Document): String? {
@@ -53,8 +85,14 @@ object DetailPageExtractor {
         return matchResult.groups[1]?.value
     }
 
-    private fun extractCoverType(detailPage: Document): String?
-            = null
+    private fun extractCoverType(detailPage: Document): String? {
+        return detailPage.applyToContentInformation {
+            filter {
+                PAGE_EXTRACTION_REGEX.containsMatchIn(it.nextSibling().toString())
+            }
+                    .map { it.text().dropLast(1) }
+        }.firstOrNull()
+    }
 
     private fun extractDescription(detailPage: Document): String? {
         val descriptionContainer = detailPage
@@ -88,6 +126,27 @@ object DetailPageExtractor {
         }
                 .firstOrNull()
                 ?.toIntOrNull()
+    }
+
+    private fun extractPrice(detailPage: Document): CurrencyValue? {
+        val rawPriceString = detailPage.select(".swatchElement.selected .a-color-price")
+                ?.text()
+                ?.trim()
+                ?: return null
+
+        val matchResult = PRICE_EXTRACTION_REGEX.find(rawPriceString) ?: return null
+
+        val currency = matchResult.groups[1]?.value ?: return null
+        val priceString = matchResult.groups[2]?.value ?: return null
+
+        val price = try {
+            NumberFormat.getNumberInstance(Locale.GERMANY).parse(priceString)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            return null
+        }
+
+        return CurrencyValue(price.toDouble(), currency)
     }
 
     private fun extractPublisher(detailPage: Document): String? {
