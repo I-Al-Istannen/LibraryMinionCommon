ecbb5eed453f01640ded966f48ae9d344ac0e912
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-01-07 11:17:14 +0100

    Completed the goodreads detail extractor

--- src/main/kotlin/me/ialistannen/libraryminioncommon/fetching/goodreads/DetailPageExtractor.kt
+++ src/main/kotlin/me/ialistannen/libraryminioncommon/fetching/goodreads/DetailPageExtractor.kt
@@ -10,11 +10,20 @@ import org.jsoup.nodes.Document
  */
 object DetailPageExtractor {
 
+    private val PUBLISHER_EXTRACT_REGEX = Regex("by\\s*(.+?)\\s*\\(")
+
     fun extractFromDetailPage(detailPage: Document): BookInformation? {
         return try {
             BookInformation(
                     title = extractTitle(detailPage),
-                    isbn = extractIsbn(detailPage)
+                    isbn = extractIsbn(detailPage),
+                    rating = extractRating(detailPage),
+                    pageCount = extractPageCount(detailPage),
+                    description = extractDescription(detailPage),
+                    language = extractLanguage(detailPage),
+                    publisher = extractPublisher(detailPage),
+                    coverType = extractCoverType(detailPage),
+                    author = extractAuthors(detailPage)
             )
         } catch (e: Exception) {
             e.printStackTrace()
@@ -26,8 +35,88 @@ object DetailPageExtractor {
             detailPage.getElementById("bookTitle").text()
 
     private fun extractIsbn(detailPage: Document): Isbn {
-        val isbnString = detailPage.getElementsByAttributeValue("itemprop", "isbn").text()
+        val isbnString = (detailPage.getItemPropString("isbn")
+                ?: throw IllegalArgumentException("Isbn element not found"))
+
         return IsbnParser.parseIsbn(isbnString)
                 ?: throw IllegalArgumentException("Isbn not valid: '$isbnString'")
+    }
+
+    private fun extractRating(detailPage: Document): Double? {
+        return detailPage.getItemPropString("ratingValue")
+                ?.toDouble()
+                ?.div(5)
+    }
+
+    private fun extractPageCount(detailPage: Document): Int? {
+        return detailPage.getItemPropString("numberOfPages")
+                ?.split(" ")
+                ?.firstOrNull()
+                ?.toInt()
+    }
+
+    private fun extractDescription(detailPage: Document): String? {
+        val descriptionContainer = detailPage.getElementById("description") ?: return null
+
+        val description = descriptionContainer
+                .getElementsByTag("span")
+                .getOrNull(1) ?: return null
+
+        return description.text()
+    }
+
+    private fun extractLanguage(detailPage: Document): String? =
+            detailPage.getItemPropString("inLanguage")
+
+    private fun extractPublisher(detailPage: Document): String? {
+        @Suppress("LoopToCallChain")
+        for (element in detailPage.select("#details > div:nth-child(2)")) {
+            val result = PUBLISHER_EXTRACT_REGEX.find(element.text()) ?: continue
+
+            return result.groups[1]?.value
+        }
+
+        return null
+    }
+
+    private fun extractCoverType(detailPage: Document): String?
+            = detailPage.getItemPropString("bookFormat")
+
+    private fun extractAuthors(detailPage: Document): List<Pair<String, String>> {
+        val result: MutableList<Pair<String, String>> = mutableListOf()
+
+        var lastAuthorName: String? = null
+
+        for (authorElement in detailPage.getElementsByClass("authorName")) {
+            // found an author with a role (e.g. Illustrator)
+            if (authorElement.hasClass("role")) {
+                val role = authorElement.text()
+                        .replace("(", "")
+                        .replace(")", "")
+                        .trim()
+                result.add(lastAuthorName!! to role)
+                lastAuthorName = null
+                continue
+            }
+
+            // found an author without a role
+            if (lastAuthorName != null) {
+                result.add(lastAuthorName to "Autor")
+            }
+            lastAuthorName = authorElement.text()
+        }
+
+        // the last author had no role
+        if (lastAuthorName != null) {
+            result.add(lastAuthorName to "Autor")
+        }
+
+        return result
+    }
+
+    private fun Document.getItemPropString(key: String): String? {
+        return getElementsByAttributeValue("itemProp", key)
+                .firstOrNull()
+                ?.text()
     }
 }
